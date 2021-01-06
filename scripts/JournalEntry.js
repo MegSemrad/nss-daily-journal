9897f0b5889e38fd11324ef1a20f4d3269294c1a
MegSemrad <mlsemrad753@gmail.com>
2021-01-06 14:00:08 -0600

    added study notes

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,7 +1,3 @@
-/*
- *  Purpose: To render a single journal entry as an
- *           HTML representation of the data
- */
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
@@ -9,7 +5,6 @@ export const JournalEntryComponent = (entry) => {
             <div class="journalConcept">${entry.concept}</div>
             <div class="journalEntry">${entry.entry}</div>
             <div class="journalMood">${entry.mood}</div>
-            <li></li>
             <input type="submit" value="Edit" class="editButton">
             <input type="submit" value="Delete" class="deleteButton">
         </section>
@@ -17,9 +12,13 @@ export const JournalEntryComponent = (entry) => {
 }
 
 /*
-    In HTML (index.html) Did not take out hard code for the <form> as did in 
-    Martin's Aquarium because it is a form and not individual entries. 
-    Therefore beneath the <form> put a <section> that contained <div> 
-    with a class which was called in the (const entryLog = document.querySelector(".individualEntry"))
-    part on the JournalEntryList.js component 
+    - Purpose: To render a single journal entry as an HTML representation of the data
+
+
+    - In HTML (index.html) Did not take out hard code for the <form> element 
+    as did in Martin's Aquarium because it is a form and not individual entries. 
+    Therefore beneath the <form> put a <section> element that contained <div> 
+    with a class which was called in the 
+    (const contentTarget = document.querySelector(".individualEntry")) part on 
+    the JournalEntryList.js component 
 */
